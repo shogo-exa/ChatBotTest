8889510e0aa14e0271bccaca3c14a7b8180f9226
hagimori <shogo-hagimori@exa-corp.co.jp>
2017-09-05 16:54:13 +0900

    add console out

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var connector = new builder.ChatConnector({
     // appPassword: process.env.MICROSOFT_APP_PASSWORD // 環境変数より取得する
     appId: "00a02aa7-0290-4e04-a61d-3c52dcff4673",
     appPassword: "pP2X72vVeVcsWaYSi3qR5oj"
-
+}, function(){
+    console.log("Complete making connector");
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
@@ -28,5 +29,6 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 
 bot.dialog('/', function (session) {
+    console.log("send Message");
     session.send("Hello World from " + botenv );
 });
