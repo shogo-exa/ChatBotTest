261ee5434e318e590d07a1f0064d783497b6f912
hagimori <shogo-hagimori@exa-corp.co.jp>
2017-09-05 17:23:25 +0900

    delete plane

--- app.js
+++ app.js
@@ -14,10 +14,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    // appId: process.env.MICROSOFT_APP_ID,  // 環境変数より取得する
-    // appPassword: process.env.MICROSOFT_APP_PASSWORD // 環境変数より取得する
-    appId: "00a02aa7-0290-4e04-a61d-3c52dcff4673",
-    appPassword: "pP2X72vVeVcsWaYSi3qR5oj"
+    appId: process.env.MICROSOFT_APP_ID,  // 環境変数より取得する
+    appPassword: process.env.MICROSOFT_APP_PASSWORD // 環境変数より取得する
 }, function(){
     console.log("Complete making connector");
 });
