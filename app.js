d81b88931e6012c9b3ce11a91386294d53ff03b5
hagimori <shogo-hagimori@exa-corp.co.jp>
2017-09-22 13:22:29 +0900

    onイベントのサンプルを追加

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var connector = new builder.ChatConnector({
 });
 
 // ボットがメッセージを受け取るURL
-server.post('/', connector.listen()); // 例：https://xxx.co.jp/multi
+server.post('/', connector.listen()); // 例：https://xxx.co.jp/
 // server.post('/message/api', connector.listen()); 例：https://xxx.co.jp/message/api
 
 var bot = module.exports = new builder.UniversalBot(connector, [
@@ -68,3 +68,29 @@ var bot = module.exports = new builder.UniversalBot(connector, [
 bot.library(require('./Cards').createLibrary());
 bot.library(require('./MultiDialog').createLibrary());
 bot.library(require('./SendImage').createLibrary());
+
+
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded && message.membersAdded.length > 0) {
+        // Say hello
+        var isGroup = message.address.conversation.isGroup;
+        var txt = isGroup ? "Hello everyone!" : "Hello...";
+        var reply = new builder.Message()
+                .address(message.address)
+                .text(txt);
+        bot.send(reply);
+    } else if (message.membersRemoved) {
+        // See if bot was removed
+        var botId = message.address.bot.id;
+        for (var i = 0; i < message.membersRemoved.length; i++) {
+            if (message.membersRemoved[i].id === botId) {
+                // Say goodbye
+                var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Goodbye");
+                bot.send(reply);
+                break;
+            }
+        }
+    }
+});
