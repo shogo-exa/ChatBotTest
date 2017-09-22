28bd0f60390880a3bc3f0708986626f358f4b518
hagimori <shogo-hagimori@exa-corp.co.jp>
2017-09-22 13:52:58 +0900

    slack のmember_joined_channelイベントの処理サンプル

--- app.js
+++ app.js
@@ -71,26 +71,17 @@ bot.library(require('./SendImage').createLibrary());
 
 
 bot.on('conversationUpdate', function (message) {
-    if (message.membersAdded && message.membersAdded.length > 0) {
-        // Say hello
-        var isGroup = message.address.conversation.isGroup;
-        var txt = isGroup ? "Hello everyone!" : "Hello...";
+    if (message.membersAdded) {
+        var membersAdded = message.membersAdded
+            .map((m) => {
+                var isSelf = m.id === message.address.bot.id;
+                return (isSelf ? message.address.bot.name : m.name);
+            })
+            .join(', ');
+
         var reply = new builder.Message()
-                .address(message.address)
-                .text(txt);
+            .address(message.address)
+            .text('いらっしゃいませー ' + membersAdded + ' さん');
         bot.send(reply);
-    } else if (message.membersRemoved) {
-        // See if bot was removed
-        var botId = message.address.bot.id;
-        for (var i = 0; i < message.membersRemoved.length; i++) {
-            if (message.membersRemoved[i].id === botId) {
-                // Say goodbye
-                var reply = new builder.Message()
-                        .address(message.address)
-                        .text("Goodbye");
-                bot.send(reply);
-                break;
-            }
-        }
     }
 });
